a2f82543d79903e18eba085c180833da16e51d47
kidchenko <kidchenko@gmail.com>
2019-06-11 18:22:19 +0700

    first commit

---
+++ CodeSanook21/CodeSanook21-Final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSanook21_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
