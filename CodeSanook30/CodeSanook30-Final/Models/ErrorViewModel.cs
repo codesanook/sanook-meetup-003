84e5798d1e57e4e1c47defcdf711b1c91407833b
kidchenko <kidchenko@gmail.com>
2019-08-07 01:53:57 +0700

    Add Final example

---
+++ CodeSanook30/CodeSanook30-Final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSanook30_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
